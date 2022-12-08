4774504fba33cdcd5f7b14ba7ebc88355c03fc63
ununbium12 <ununbium@naver.com>
2022-12-08 13:29:34 +0900

    과제 및 그외의 것들 커밋

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,21 @@ const userNameElement = document.querySelector('.user-name');
 const localuserName = localStorage.getItem('userName');
 const userNameBtr = document.querySelector('#name');
 
+const userInformationNDElement = document.querySelector('.InFoND');
+const userInformationEMElement = document.querySelector('.InFoEM');
+const localInformationND = localStorage.getItem('userInFoND');
+const localInformationEM = localStorage.getItem('userInFoEM');
+const userInFoNDBtr = document.querySelector('#ND');
+const userInFoEmBtr = document.querySelector('#EM');
+
+const setUserInformationNDInnerHtml = (infomationND) => {
+  userInformationNDElement.innerHTML = `<span id="ND"> ${infomationND}</span>`;
+};
+
+const setUserInformationEMInnerHtml = (infomationEM) => {
+  userInformationEMElement.innerHTML = `<span id="EM"> ${infomationEM} </span>`;
+};
+
 const setUserNameInnerHtml = (name) => {
   headeruserNameElement.innerHTML = `${name}<span>님</span>`;
   userNameElement.innerHTML = `<span id="name">${name}</span><span>님</span>`;
@@ -14,6 +29,13 @@ const setUserNameInnerHtml = (name) => {
 
 if (localuserName) {
   setUserNameInnerHtml(localuserName);
+}
+
+if (localInformationND) {
+  setUserInformationNDInnerHtml(localInformationND);
+}
+if (localInformationEM) {
+  setUserInformationEMInnerHtml(localInformationEM);
 }
 
 // eslint-disable-next-line max-len
@@ -25,6 +47,37 @@ userNameBtr.onclick = () => {
 
   setUserNameInnerHtml(userName);
 };
+
+userInFoNDBtr.onclick = () => {
+  const userInFoND = prompt('학번을 입력해 주십시오');
+  const number = /[0~9]/;
+  if (userInFoND == number) {
+    localStorage.setItem('userInFoND', userInFoND);
+    setUserInformationNDInnerHtml(userInFoND);
+  } else if (userInFoND == null) {
+    alert('입력 공란');
+  } else {
+    alert('숫자만 입력');
+  }
+};
+
+userInFoEmBtr.onclick = () => {
+  const userInFoEM = prompt('이메일을 입력해 주십시오');
+  const email = /^[\w]{4,}@[\w]+(\.[\w]+){1,3}$/;
+  if (userInFoEM == email) {
+    localStorage.setItem('userInFoEM', userInFoEM);
+    setUserInformationEMInnerHtml(userInFoEM);
+  } else if (userInFoEM == null) {
+    alert('입력 공란');
+  } else {
+    alert('이메일 양식에 맞춰 입력');
+  }
+};
+
+// 아래 3개 코드는 입력된 값을 F5로 초기화 시킬 수 있게 하는 코드
+// delete localStorage.userName;
+// delete localStorage.userInFoND;
+// delete localStorage.userInFoEM;
 
 // console.log(userNameElement);
 // console.log(userNameElement.textContent);
